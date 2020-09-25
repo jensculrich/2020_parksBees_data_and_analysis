d6582ca04f9343d41ffa487cc131ed47cc57dc96
Jens Johnson <jensj27@gmail.com>
2020-09-25 15:52:53 -0700

    fixed so that data is gathered long format with a new column for month. added species richness sum.

--- floral_abundance_and_land_v_insects.R
+++ floral_abundance_and_land_v_insects.R
@@ -10,39 +10,40 @@ str(site_gps_data)
 # merge tables by site
 merged_df <- (floral_data %>% full_join(site_gps_data))
 
+# new column (key) = month, new column (value) = number of floral units
+merged_df_2 <- merged_df %>%
+  rename(july = number_of_floral_units_july) %>%
+  rename(august = number_of_floral_units_august) %>%
+  gather(key = month, value = number_floral_units, 
+         july:august)
+
+# merged_df_summarised
 # calculate sum of all floral units per site per month
 # edit: sum of all values "number_of_floral_units_x"
 # for which the value is greater than 0.
-merged_df_summarised <- merged_df %>%
-  group_by(site) %>%
-  mutate(total_flowers_july = 
-              sum(number_of_floral_units_july), 
-            total_flowers_august = 
-              sum(number_of_floral_units_august))
+merged_df_summarised <- merged_df_2 %>%
+  group_by(site, month) %>%
+  mutate(flowers_per_sq_m = 
+              sum(number_floral_units)/15)
 
 P <- ggplot(merged_df_summarised, 
-            aes(x = management, y = total_flowers_august)) +
+            aes(x = management, y = flowers_per_sq_m, fill = month)) +
   geom_boxplot()
 P
 
-## would like to plot both months on the same y axis
-## new column = month, new column = total flowers
-## rows = total_flowers_july or total_flowers_august
-merged_df_summarised_2 <- merged_df_summarised %>%
-  gather(key = month, value = total_flowers, 
-         total_flowers_july:total_flowers_august) %>%
-  mutate(floral_units_per_sq_m = total_flowers/15)
+merged_df_summarised_2 <- merged_df_summarised %>% 
+  group_by(site, month) %>% 
+  # remove rows where plant was flowering at site in 
+  # other month but not the month specified in 'month'
+  filter(number_floral_units != 0)  %>% 
+  # count number of rows to get floral species richness
+  add_count() %>%
+  rename(floral_richness = n)
 
-# make (mutate) a new column that has flowers/15
+R <- ggplot(merged_df_summarised_2, 
+            aes(x = management, y = floral_richness)) +
+  geom_boxplot(aes(fill = forcats::fct_rev(month)))
+R
 
-# now try to plot with fill by month
-P <- ggplot(merged_df_summarised_2, 
-            aes(x = management, y = floral_units_per_sq_m, fill = month)) +
-  geom_boxplot()
-P
-
-## richness = sum rows for each site for which
-## "number_of_floral_units_x" =/= 0 (could be negative or positive)
-
-## edit or simulate: China creek Park, everett crowley park, campbell rd july.
+# edit or simulate: China creek Park, everett crowley park, campbell rd july.
 
