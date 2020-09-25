ece0f48c6e2a5d3215cce2fc7ed18a2fec9c6890
Jens Johnson <jensj27@gmail.com>
2020-09-25 16:32:15 -0700

    added correction to not subtract floral abundances for "-1" values which represent presnt at site but not found in any plots.

--- floral_abundance_and_land_v_insects.R
+++ floral_abundance_and_land_v_insects.R
@@ -19,12 +19,13 @@ merged_df_2 <- merged_df %>%
 
 # merged_df_summarised
 # calculate sum of all floral units per site per month
-# edit: sum of all values "number_of_floral_units_x"
+# edit: sum of all values "number_floral_units_x"
 # for which the value is greater than 0.
 merged_df_summarised <- merged_df_2 %>%
   group_by(site, month) %>%
   mutate(flowers_per_sq_m = 
-              sum(number_floral_units)/15)
+              sum(number_floral_units)/15) 
+# counted 15 plots so divide by 15 to get per m^2
 
 P <- ggplot(merged_df_summarised, 
             aes(x = management, y = flowers_per_sq_m, fill = month)) +
@@ -40,10 +41,22 @@ merged_df_summarised_2 <- merged_df_summarised %>%
   add_count() %>%
   rename(floral_richness = n)
 
-R <- ggplot(merged_df_summarised_2, 
+Q <- ggplot(merged_df_summarised_2, 
             aes(x = management, y = floral_richness)) +
   geom_boxplot(aes(fill = forcats::fct_rev(month)))
-R
+Q
+
+merged_df_summarised_3 <- merged_df_summarised_2 %>%
+  group_by(site, month) %>%
+  filter(number_floral_units > -1) %>%
+  mutate(number_floral_units = na_if(number_floral_units, -1)) %>%
+  mutate(flowers_per_sq_m = 
+           sum(number_floral_units)/15)
+
+P2 <- ggplot(merged_df_summarised_3, 
+            aes(x = management, y = flowers_per_sq_m, fill = month)) +
+  geom_boxplot()
+P2
 
 # edit or simulate: China creek Park, everett crowley park, campbell rd july.
 
